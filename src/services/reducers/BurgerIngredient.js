74b9d4f164979ede72f3e2f5dbf8eebd97770aa9
Алёна <113091164+AlionaKulikova@users.noreply.github.com>
2023-02-16 08:24:56 +0300

    Update BurgerIngredient.js

--- src/services/reducers/BurgerIngredient.js
+++ src/services/reducers/BurgerIngredient.js
@@ -1,17 +1,21 @@
 import { ADD_INGREDIENT_DETAL } from "../actions/BurgerIngredient";
 
-const initialState = {};
+const initialState =  {};
 
 export const dataIngredientsReducer = (state = initialState, action) => {
 
     switch (action.type) {
+
         case ADD_INGREDIENT_DETAL: {
 
-            const dataIngredient = action.dataIngredient;
+           const dataIngredient= action.dataIngredient;
 
-            return {
-                dataIngredient,
-            };
+         return {
+            dataIngredient,
+         };
         }
+
+        default:
+            return state;
     }
 }
